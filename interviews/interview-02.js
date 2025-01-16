4bedd843a6e45a27dc9f90ab1951753dbc9a916c
imoncoc <imoncoc0@gmail.com>
2025-01-16 19:28:52 +0600

    h

---
+++ interviews/interview-02.js
@@ -0,0 +1,46 @@
+
+// 1. ngOnInit?
+// 2. constructor?
+// 3. constructor and ngOnInit who call first?
+// 4. Translator
+// 5. pipe type?
+// 6. Different between pure and impure pipe?
+// 7. how data store ?
+// 8. Subject behavioursubject difference?
+// 9. same task how to do without Subject or behavioursubject? (evenEmitter)
+// 10. single instance service?
+// 11. Debounce?
+// 12. LocalStorage, sessionStorage, cookies?
+// 13. Why cookies more secure then LocalStorage, sessionStorage?
+// 14. What is guard or how to protect admin, user?
+// 15. If not use provideIn
+// 16. Js Hosting?
+// 17. What is Scope?
+// 18. How works js Engine?
+// 19. Interceptor?
+// 20. Promise?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 20. How avoid memory leak in Angular?
+// 19. Resolver?
+// token?
+// Good knowledge of route
